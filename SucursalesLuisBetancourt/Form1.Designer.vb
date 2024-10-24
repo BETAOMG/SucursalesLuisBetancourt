85f5886e8bcd9403d5781ac801c5968ba73c363a
BETAOMG <luisdavidbch@gmail.com>
2024-10-23 19:35:58 -0500

    Agregar archivos de proyecto.

---
+++ SucursalesLuisBetancourt/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label3 = New Label()
+        TextBox4 = New TextBox()
+        Button1 = New Button()
+        Label2 = New Label()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        Label4 = New Label()
+        Label5 = New Label()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(183, 96)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(128, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Cantidad de sucursales"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(328, 93)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(63, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(146, 136)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.ScrollBars = ScrollBars.Vertical
+        TextBox2.Size = New Size(309, 64)
+        TextBox2.TabIndex = 3
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(297, 227)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(39, 23)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(132, 230)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(159, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "Sucursal con mayores ventas"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(355, 227)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 7
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(183, 366)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 8
+        Button1.Text = "Calcular"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(131, 273)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(160, 15)
+        Label2.TabIndex = 9
+        Label2.Text = "Sucursal con menores ventas"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(297, 270)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(39, 23)
+        TextBox5.TabIndex = 11
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(355, 270)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(100, 23)
+        TextBox6.TabIndex = 10
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(297, 315)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(158, 23)
+        TextBox7.TabIndex = 14
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(131, 318)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(161, 15)
+        Label4.TabIndex = 12
+        Label4.Text = "Total de ventas de la empresa"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 20F)
+        Label5.Location = New Point(183, 39)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(258, 37)
+        Label5.TabIndex = 15
+        Label5.Text = "Ventas de sucursales"
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(274, 366)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 16
+        Button2.Text = "Otro"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(366, 366)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 17
+        Button3.Text = "Salir"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(612, 458)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Label5)
+        Controls.Add(TextBox7)
+        Controls.Add(Label4)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox6)
+        Controls.Add(Label2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+
+End Class
---
+++ SucursalesLuisBetancourt/Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'CantidadSucursales Cantidad de sucursales
+        'CantidadVentas Cantidad de ventas
+        'VentaSucursales Venta por sucursales
+        'i Indice de ciclo
+        'j Indice de ciclo
+        'iMayor Indice de la sucursal con mayores ventas
+        'iMenor Indice de la sucursal con menores ventas
+        'SumaVentas Suma de las ventas
+
+        Dim CantidadSucursales, CantidadVentas, iMayor, iMenor, SumaVentas As Integer
+        CantidadSucursales = TextBox1.Text
+        Dim VentaSucursales(CantidadSucursales) As Integer
+
+        iMayor = 1
+        iMenor = 1
+
+        TextBox2.Text = Chr(9) + "Ventas por sucursal" + vbCrLf + vbCrLf
+
+        For i = 1 To CantidadSucursales
+            CantidadVentas = Val(InputBox("Numero de ventas de la sucursal " + Str(i)))
+            For j = 1 To CantidadVentas
+                VentaSucursales(i) = VentaSucursales(i) + Val(InputBox("Venta " + Str(j) + " sucursal " + Str(i)))
+            Next
+            If VentaSucursales(i) < VentaSucursales(iMenor) Then
+                iMenor = i
+            End If
+            If VentaSucursales(i) > VentaSucursales(iMayor) Then
+                iMayor = i
+            End If
+            SumaVentas = SumaVentas + VentaSucursales(i)
+            TextBox2.Text = TextBox2.Text + "Sucursal " + Str(i) + ": " + Str(VentaSucursales(i)) + vbCrLf
+        Next
+
+        TextBox3.Text = Str(iMayor)
+        TextBox4.Text = Str(VentaSucursales(iMayor))
+        TextBox5.Text = Str(iMenor)
+        TextBox6.Text = Str(VentaSucursales(iMenor))
+        TextBox7.Text = Str(SumaVentas)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+        TextBox7.Text = ""
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+End Class
